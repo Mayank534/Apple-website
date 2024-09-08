f1a03dc93ec26fab8afdda3860477401a65e8979
Mayank534 <mayanka22@iitk.ac.in>
2024-09-09 03:32:25 +0530

    deployed website

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/apple_website/',
   plugins: [react(), sentryVitePlugin({
     org: "jsm-x9",
     project: "javascript-react"
